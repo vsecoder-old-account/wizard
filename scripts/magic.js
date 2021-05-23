b110f716a265bead9d80df9e9abb7a120d840041
Всеволод <63463168+htmlcssphpjs@users.noreply.github.com>
2021-05-23 20:56:28 +0300

    #2

--- scripts/magic.js
+++ scripts/magic.js
@@ -1,5 +1,8 @@
 // In this file MAGIC
 
+delete_words = "как зачем почему когда ибо но не нет да потому что потому-что видимо возможно иногда сейчас надо и а";
+delete_words = delete_words.split(" ");
+
 module.exports.func = function (read, finde) {
   if (read == undefined) {
     return;
@@ -15,17 +18,40 @@ module.exports.func = function (read, finde) {
           var web = read[i].web;
           var q_el = read[i].web.description.split(" ");
           for (let m = 0; m < q_el.length; m++) {
-            var m_el = q_el[m].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+            var m_el = q_el[m].replace(/[.,\/#!$%?\^&\*;:{}=\-_`~()]/g, "");
             m_el = m_el.toLowerCase();
             element = element.toLowerCase();
-            console.log(element + ' | ' + m_el + ' = ' + finding);
-            if (element == m_el && finding <= 9) {
-              arr.push({
-                "title": web.title,
-                "url": web.url,
-                "description": web.description
-              });
-              finding++;
+            //console.log(element + ' | ' + m_el + ' = ' + finding);
+            for (let a = 0; a < delete_words.length; a++) {
+              const word = delete_words[a];
+              if (m_el == word || element == word) {
+                //
+              } else {
+                var arr_to = arr;
+                if (arr.length != 0) {
+                  for (let d = 0; d < arr_to.length; d++) {
+                    const arr_el = array[d].description;
+                    console.log(arr_el);
+                    if (element == m_el && finding <= 9 && web.description != arr_el) {
+                      arr.push({
+                        "title": web.title,
+                        "url": web.url,
+                        "description": web.description
+                      });
+                      finding++;
+                    }
+                  }
+                } else {
+                  if (element == m_el && finding <= 9) {
+                    arr.push({
+                      "title": web.title,
+                      "url": web.url,
+                      "description": web.description
+                    });
+                    finding++;
+                  }
+                }
+              }
             }
           }
 
