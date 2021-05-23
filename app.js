f76e9fd01343517d4fc6a4c9e73ed3856ff9cf9d
Всеволод <63463168+htmlcssphpjs@users.noreply.github.com>
2021-05-23 20:54:39 +0300

    #2

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var express = require('express'),
   port = process.env.PORT || 5000,
   fire = require('./scripts/bd.js'),
   find = require('./scripts/find.js'),
-  magic = require('./scripts/magic.js');
+  magic = require('./scripts/magic.js'),
+  request = require('request');
+
+//const cheerio = require("cheerio");
 
 // import and use handlebars
 var handlebars = require('express-handlebars')
@@ -20,6 +23,8 @@ app.all('*', function(req, res, next) {
 	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
   console.log(`IP ${ip} open ${fullUrl}`);
   req.setMaxListeners(0);
+  res.setHeader("Content-Type", "text/html");
+  res.charset = 'utf-8';
   next();
 });
 
@@ -36,9 +41,17 @@ app.get(['/', '/index.html', '/index.js', '/index.php', '/index.py', '//'], func
     if (arr.length == 0) {
       arr.push({
         "title": "Wizard",
-        "url": "https://yandex.ru/?q=" + finde,
+        "url": "https://yandex.ru/search/?lr=10&text=" + finde,
         "description": "Мы не нашли подходящего результата у себя, воспользуйтесь Яндексом"
       });
+      //console.log("https://yandex.ru/search/?lr=10&text=" + finde);
+      //request("https://yandex.ru/search/?text=" + finde, function (err, res, body) {
+      //  console.log(body);
+      //  const $ = cheerio.load(body);
+      //  for (let i = 0; i < 5; i++) {
+      //    console.log($('.yuRUbf a', body)[i].attribs.href);
+      //  }
+      //});
     }
   }
   const end = new Date().getTime();
@@ -48,8 +61,43 @@ app.get(['/', '/index.html', '/index.js', '/index.php', '/index.py', '//'], func
 });
 
 // add
-app.get('/add', function(req, res){
-  res.render('add');
+app.get('/add', function (req, res) {
+  var q = req.query;
+  var url = q.url;
+  var res = res;
+  var er_title = 'Сайт не удалось добавить, проверьте наличие тегов title и meta на сайте!';
+  if (url != undefined) {
+    try {
+      request(url, function (err, resu, body) {
+        if (err || resu.statusCode != 200) {
+          res.render('add', { text: 'Сайт не удалось добавить, код ошибки: ' + resu.statusCode + ' ' + err });
+        }
+        var title = body.match(/<title[^>]*>(.*?)<\/title>/gis);
+        var description = body.match(/<meta property=\"og:description\" content=\"(.*?)\"*>/gis);
+        if (title == null || description == null) {
+          res.render('add', { text: er_title });
+        } else {
+          title = title[0].replace(/<title[^>]*>/gis, '');
+          title = title.replace(/<\/title>/gis, '');
+          description = description[0].replace(/<meta property=\"og:description\" content=\"/gis, '');
+          description = description.replace(/\"\/>/gis, '');
+          var result = {
+            title: title,
+            url: url,
+            description: description
+          };
+          console.log(result);
+          fire.set_bd(result.title, url, result.description);
+          res.render('add', { text: 'Сайт удачно добавлен!' });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      res.render('add', { text: er_title });
+    }
+  } else {
+    res.render('add');
+  }
 });
 
 // admin
